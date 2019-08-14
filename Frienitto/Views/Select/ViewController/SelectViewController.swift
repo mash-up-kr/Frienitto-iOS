16957b07cedec5675c195be59e76a6093423cf0c
Gaon Kim <gaon.kim.dev@gmail.com>
2019-08-14 22:06:07 +0900

    Add Lottie animation on Select View

--- Frienitto/Views/Select/ViewController/SelectViewController.swift
+++ Frienitto/Views/Select/ViewController/SelectViewController.swift
@@ -7,10 +7,34 @@
 //
 
 import UIKit
+import Lottie
 
 class SelectViewController: UIViewController {
     
+    @IBOutlet private weak var contentView: UIView! {
+        didSet {
+            contentView.alpha = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addLottieView()
+    }
+    
+    private func addLottieView() {
+        let selectLottieView = AnimationView(name: "data")
+        selectLottieView.frame = view.bounds
+        selectLottieView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(selectLottieView)
+
+        selectLottieView.loopMode = .playOnce
+        selectLottieView.play() { _ in
+            selectLottieView.removeFromSuperview()
+            
+            UIView.animate(withDuration: 0.3, delay: 0.3, animations: {
+                self.contentView.alpha = 1
+            })
+        }
     }
 }
