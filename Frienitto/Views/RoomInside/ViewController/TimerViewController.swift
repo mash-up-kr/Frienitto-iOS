cae9a7d0023e735e6b51f999d20b81a023632eb0
Gaon Kim <gaon.kim.dev@gmail.com>
2019-08-17 22:18:20 +0900

    Create TimerViewController

---
+++ Frienitto/Views/RoomInside/ViewController/TimerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TimerViewController.swift
+//  Frienitto
+//
+//  Created by Gaon Kim on 2019/08/17.
+//  Copyright © 2019 Mash-Up. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
