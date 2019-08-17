e0b4de3867599eecdc001d0882ca285fc82a887b
Gaon Kim <gaon.kim.dev@gmail.com>
2019-08-17 18:01:09 +0900

    Update models related to room

--- Frienitto/Models/RetrieveRoomDetailModel.swift
+++ Frienitto/Models/RetrieveRoomDetailModel.swift
@@ -9,20 +9,7 @@
 import Foundation
 
 struct RetrieveRoomDetailModel: Codable {
-    struct Data: Codable {
-        let id: Int
-        let title: String
-        let status: String
-        let expiresDate: String
-        let participants: [User]
-        
-        enum CodingKeys: String, CodingKey {
-            case id, title, status, participants
-            case expiresDate = "expires_date"
-        }
-    }
-    
     let code: Int
     let msg: String
-    let data: Data
+    let data: Room
 }
