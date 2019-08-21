426e97ba39601ff3f7675bb909a43861fa66b2ff
Gaon Kim <gaon.kim.dev@gmail.com>
2019-08-22 01:31:29 +0900

    Refactor networking functions of `LoginViewController`

--- Frienitto/Views/Login/ViewController/LoginViewController.swift
+++ Frienitto/Views/Login/ViewController/LoginViewController.swift
@@ -21,68 +21,108 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     // MARK: - Action
     
     @IBAction func loginButtonAction(_ sender: Any) {
-        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+        configureWarningLabels()
         
-        if email.isEmpty || password.isEmpty {
-            warningEmailLabel.isHidden = !email.isEmpty
-            warningPwLabel.isHidden = !password.isEmpty
-            return
-        }
+        guard
+            let email = emailTextField.text,
+            let password = passwordTextField.text,
+            !email.isEmpty,
+            !password.isEmpty else { return }
         
-        warningEmailLabel.isHidden = true
-        warningPwLabel.isHidden = true
-        
-        let provider = FrienttoProvider()
-        
-        showActivityIndicator()
-        provider.signIn(email: email, password: password, completion: { [weak self] signInModel in
-            UserDefaults.standard.set(signInModel.data.token, forKey: "authorizationToken")
-            if let data = try? JSONEncoder().encode(signInModel.data.user) {
-                UserDefaults.standard.set(data, forKey: "userInfo")
-            }
-            
-            provider.retrieveRoomList(completion: { [weak self] roomListModel in
-                let nextViewController: UIViewController
-                
-                if roomListModel.data.isEmpty {
-                    guard let mainViewController = UIStoryboard.instantiate(MainViewController.self, name: "Main") else { return }
-                    nextViewController = mainViewController
-                } else {
-                    guard let mainListViewController = UIStoryboard.instantiate(MainListViewController.self, name: "Main") else { return }
-                    
-                    nextViewController = mainListViewController
-                    mainListViewController.rooms = roomListModel.data
-                    mainListViewController.user = signInModel.data.user
-                }
-                
-                self?.navigationController?.setViewControllers([nextViewController], animated: true)
-
-            }, failure: { error, _ in
-                guard let alertViewController = UIStoryboard.instantiate(OneButtonAlertViewController.self, name: "Login") else { return }
-                alertViewController.delegate = self
-                alertViewController.configure(status: .failureNetwork)
-                
-                self?.present(alertViewController, animated: true, completion: nil)
-            })
-            
-        }, failure: { error, errorResponse in
-            guard let alertViewController = UIStoryboard.instantiate(OneButtonAlertViewController.self, name: "Login") else { return }
-            alertViewController.delegate = self
-            alertViewController.configure(status: .noUser)
-            
-            self.present(alertViewController, animated: true, completion: nil)
-        })
+        signIn(email: email, password: password)
     }
     
     @IBAction func backButtonAction(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+private extension LoginViewController {
+    func configureWarningLabels() {
+        warningEmailLabel.isHidden = !(emailTextField.text?.isEmpty ?? false)
+        warningPwLabel.isHidden = !(passwordTextField.text?.isEmpty ?? false)
+    }
+    
+    func signIn(email: String, password: String) {
+        let provider = FrienttoProvider()
+        showActivityIndicator()
+        
+        provider.signIn(
+            email: email,
+            password: password,
+            completion: { [weak self] signInModel in
+                UserDefaults.standard.set(signInModel.data.token, forKey: "authorizationToken")
+                if let data = try? JSONEncoder().encode(signInModel.data.user) {
+                    UserDefaults.standard.set(data, forKey: "userInfo")
+                }
+                
+                self?.retrieveUserRoom(signInModel: signInModel)
+                
+            },
+            failure: { [weak self] error, errorResponse in
+                self?.presentAlertViewController(status: .noUser)
+            }
+        )
+    }
+    
+    func retrieveUserRoom(signInModel: SignInModel) {
+        let provider = FrienttoProvider()
+        showActivityIndicator()
+        
+        provider.retrieveUserRoom(
+            completion: { [weak self] roomListModel in
+                guard let self = self else { return }
+                
+                let rooms = roomListModel.data
+                let user = signInModel.data.user
+                
+                let nextViewController = self.getNextViewController(rooms: rooms, user: user)
+            
+                self.navigationController?.setViewControllers([nextViewController], animated: true)
+            },
+            failure: { [weak self] error, _ in
+                self?.presentAlertViewController(status: .failureNetwork)
+            }
+        )
+    }
+    
+    func getNextViewController(rooms: [Room], user: User) -> UIViewController {
+        let nextViewController: UIViewController
+        
+        if rooms.isEmpty {
+            nextViewController = self.getMainViewController()
+        } else {
+            nextViewController = self.getMainListViewController(rooms: rooms, user: user)
+        }
+        
+        return nextViewController
+    }
+    
+    func getMainViewController() -> UIViewController {
+        guard let mainViewController = UIStoryboard.instantiate(MainViewController.self, name: "Main") else { return UIViewController() }
+        return mainViewController
+    }
+    
+    func getMainListViewController(rooms: [Room], user: User) -> UIViewController {
+        guard let mainListViewController = UIStoryboard.instantiate(MainListViewController.self, name: "Main") else { return UIViewController() }
+        
+        mainListViewController.rooms = rooms
+        mainListViewController.user = user
+        
+        return mainListViewController
+    }
+    
+    func presentAlertViewController(status: OneButtonAlertViewController.Status) {
+        guard let alertViewController = UIStoryboard.instantiate(OneButtonAlertViewController.self, name: "Login") else { return }
+        alertViewController.delegate = self
+        alertViewController.configure(status: status)
+        
+        present(alertViewController, animated: true, completion: nil)
     }
 }
 
